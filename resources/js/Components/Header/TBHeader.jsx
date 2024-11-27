7b1f075fdf610b5164e670b739c2d0914993558a
Menura Basitha Andrahennedi <149805815+MenuraAndrahennedi@users.noreply.github.com>
2024-11-27 10:55:50 +0530

    update-paths

--- resources/js/Components/Header/TBHeader.jsx
+++ resources/js/Components/Header/TBHeader.jsx
@@ -1,32 +1,59 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { Link } from '@inertiajs/react';
-import '../../../css/style.scss';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "@inertiajs/react";
+import "../../../css/style.scss";
 
-import Logo from '../../assets/Logo.png';
+import Logo from "../../assets/Images/Logo.png";
 
 const TBHeader = () => {
-  return (
-    <header>
-        <div className='container py-3'>
-            <Navbar expand="lg" className="bg-dark navbar-dark">
-                <Navbar.Brand as={Link} to="/" className = 'logo'><img src={Logo} alt="Logo" className="logo-img" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link as={Link} to="/" className = 'nav-link'>Home</Nav.Link>
-                    <Nav.Link as={Link} to="/about"className = 'nav-link'>About US</Nav.Link>
-                    <Nav.Link as={Link} to="/contact" className = 'nav-link'>Contact US</Nav.Link>
-                    <Nav.Link as={Link} to="/browse" className = 'nav-link'>Browse EVENT</Nav.Link>
-                    <Nav.Link as={Link} to="/TBaccount" className = 'nav-link'>TB Account</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    </header>
-  )
+    return (
+        <header>
+            <div className="container py-3">
+                <Navbar expand="lg" className="bg-dark navbar-dark">
+                    <Navbar.Brand as={Link} to="/" className="logo">
+                        <img src={Logo} alt="Logo" className="logo-img" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/" className="nav-link">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/about"
+                                className="nav-link"
+                            >
+                                About US
+                            </Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/contact"
+                                className="nav-link"
+                            >
+                                Contact US
+                            </Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/browse"
+                                className="nav-link"
+                            >
+                                Browse EVENT
+                            </Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/TBaccount"
+                                className="nav-link"
+                            >
+                                TB Account
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        </header>
+    );
+};
 
-}
-
-export default MainHeader
+export default TBHeader;
