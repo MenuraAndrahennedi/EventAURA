7e49f5cd4d7cc1d8746cf9a66aa8e240c87f96ad
Menura Basitha Andrahennedi <149805815+MenuraAndrahennedi@users.noreply.github.com>
2024-11-29 13:00:59 +0530

    update-end

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -10,13 +10,11 @@ class Event extends Model
     /** @use HasFactory<\Database\Factories\EventFactory> */
     use HasFactory;
 
-    protected $table = 'event';
-
     protected $fillable = [
-        'name','date','startTime','endTime','description','location','image','city','venue','artists',
-        'agenda_pdf','organizer','event_video','bronze_ticket_count',
-        'golden_ticket_count','silver_ticket_count','event_status',
-        'bronze_ticket_price','golden_ticket_price','silver_ticket_price',
-        'event_host_id'
+        'name', 'date', 'startTime', 'endTime', 'description', 'location', 'image', 'city', 'venue', 'artists',
+        'agenda_pdf', 'organizer', 'event_video', 'bronze_ticket_count',
+        'golden_ticket_count', 'silver_ticket_count', 'event_status',
+        'bronze_ticket_price', 'golden_ticket_price', 'silver_ticket_price',
+        'event_host_id',
     ];
 }
--- resources/js/Components/Header/MainHeader.jsx
+++ resources/js/Components/Header/MainHeader.jsx
@@ -3,15 +3,13 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "@inertiajs/react";
 
-import '../../../css/style.scss';
-import Logo from '../../assets/Images/Logo.png';
-
+import "../../../css/style.scss";
+import Logo from "../../assets/Images/Logo.png";
 
 const MainHeader = () => {
     return (
         <header>
-            <div classNmae="container py-3">
-            <div className='container py-3'>
+            <div className="container py-3">
                 <Navbar expand="lg" className="bg-dark navbar-dark">
                     <Navbar.Brand as={Link} to="/" className="logo">
                         <img src={Logo} alt="Logo" className="logo-img" />
@@ -59,27 +57,3 @@ const MainHeader = () => {
 };
 
 export default MainHeader;
-
-// const MainHeader = () => {
-//     return (
-//         <header>
-//             <div className='container py-3'>
-//                 <Navbar expand="lg" className="bg-dark navbar-dark">
-//                     <Navbar.Brand as={Link} href="/" className='logo'><img src={Logo} alt="Logo" className="logo-img" /></Navbar.Brand>
-//                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//                     <Navbar.Collapse id="basic-navbar-nav">
-//                         <Nav className="ms-auto">
-//                             <Nav.Link as={Link} href="/" className='nav-link'>Home</Nav.Link>
-//                             <Nav.Link as={Link} href="/about" className='nav-link'>About US</Nav.Link>
-//                             <Nav.Link as={Link} href="/contact" className='nav-link'>Contact US</Nav.Link>
-//                             <Nav.Link as={Link} href="/browse" className='nav-link'>Browse EVENT</Nav.Link>
-//                             <Nav.Link as={Link} href="/create" className='nav-link'>Create EVENT</Nav.Link>
-//                         </Nav>
-//                     </Navbar.Collapse>
-//                 </Navbar>
-//             </div>
-//         </header>
-//     )
-// }
-
-// export default MainHeader
