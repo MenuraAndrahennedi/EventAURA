29b6ae3664905ac0a4024a517b6ae952f8cb1ae8
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-03-08 08:42:34 +0530

    update sidebar.jsx

--- resources/js/Components/SideBar/EHSideBar.jsx
+++ resources/js/Components/SideBar/EHSideBar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "@inertiajs/react";
+import '../../../css/Profile.scss';
 
 const EHSideBar = () => {
   return (
-    <div>EHSideBar</div>
+    <main>
+     <div className="border-0 shadow card sidebar">
+        <div className="p-4 card-body">
+                <ul className="sidebar-menu">
+                    <li><Link to='/EHaccount' className={location.pathname === '/EHaccount' ? 'active-link' : ''}>Profile Details</Link></li>
+                    <li><Link to='/EHOngoing' className={location.pathname === '/EHOngoing' ? 'active-link' : ''}>Event History</Link></li>
+                    <li><Link to='/EHChangePW' className={location.pathname === '/EHChangePW' ? 'active-link' : ''}>Change Password</Link></li>
+                    <li><Link to='/EHSignout' className={location.pathname === '/EHSignout' ? 'active-link' : ''}>Sign Out</Link></li>
+                    <li></li>
+                </ul>
+         </div>
+      </div>
+    
+  </main>
   )
 }
 
--- resources/js/Pages/EHProfile/EHChangePw.jsx
+++ resources/js/Pages/EHProfile/EHChangePw.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "@inertiajs/react";
+import SubFooter from './../../Components/Footer/SubFooter';
+import EHHeader from './../../Components/Header/EHHeader';
+import '../../../css/Profile.scss';
+import profilelogo from '../../../js/assets/Images/Profile-logo.png';
+import EHSidebar from './../Manager/PendingRequests/HeadBar';
+
 
 const EHChangePw = () => {
   return (
-    <div>EHChangePw</div>
+    <>
+      <header>
+        <EHHeader />
+      </header>
+
+      <main>
+        <div className="container my-5">
+              <div className="border-0 shadow card">
+                <div className="text-center card-body">
+                    <h1><b>Hi Anura!</b></h1>
+                    <h4><b>Welcome to Your Profile</b></h4>
+                    <img src= {profilelogo} alt="Avatar"className="profile-avatar" />
+                </div>
+              </div>
+
+              <div className="mt-4 row">
+                  <div className="col-md-3">
+                    <EHSidebar />
+                  </div>
+                  <div className="col-md-9">
+                    <div className="border-0 shadow card">
+                      <div className="card-body profile-details">
+                        <h5 className="text-center"><b>Change Password</b></h5>
+                        <p className="text-center">@username</p>
+
+                        <div className="details-grid">
+                            <div className="detail-label">Full Name:</div>
+                            <input type="text" className="form-control" placeholder="Enter Full Name" />
+                            
+                            <div className="detail-label">Email:</div>
+                            <input type="email" className="form-control" placeholder="Enter Email" />
+
+                            <div className="detail-label">Telephone No:</div>
+                            <input type="text" className="form-control" placeholder="Enter Telephone No" />
+                        </div>
+
+                        <div className="text-end">
+                          <button className="mt-3 btn btn-secondary me-2">Cancel</button>
+                          <button className="mt-3 btn btn-primary ">Save Changes</button>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+      </main>
+      <footer>
+        <SubFooter />
+      </footer>
+    </>
   )
 }
 
