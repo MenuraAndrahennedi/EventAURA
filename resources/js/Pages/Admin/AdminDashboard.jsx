f750a55e119449ab38d5e4f181f70bda9a611308
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-04-01 10:03:05 +0530

    update admindashboard.jsx

--- resources/js/Pages/Admin/AdminDashboard.jsx
+++ resources/js/Pages/Admin/AdminDashboard.jsx
@@ -1,47 +1,95 @@
-  
 
-    // import { Link } from '@inertiajs/react';
+import React from 'react'
+import "../../../css/UserDashboard.scss"
+import { Link } from '@inertiajs/react';
 
-    // export default function CustomerDashboard() {
-    //     return (
-    //         <div>
-    //             <h1>Admin Dashboard</h1>
-    //             <form 
-    //                 id="logout-form" 
-    //                 action="/logout" 
-    //                 method="POST" 
-    //                 style={{ display: 'none' }}
-    //             >
-    //                 <input 
-    //                     type="hidden" 
-    //                     name="_token" 
-    //                     value={document.querySelector('meta[name="csrf-token"]').getAttribute('content')} 
-    //                 />
-    //             </form>
-    
-    //             <a 
-    //                 href="#" 
-    //                 onClick={(e) => {
-    //                     e.preventDefault();
-    //                     document.getElementById('logout-form').submit();
-    //                 }}
-    //             >
-    //                 Logout
-    //             </a>
-    //         </div>
-    //     );
-    // }
-    
+import Cover01 from '../../assets/Images/Tile 1.jpg';
+import Cover02 from '../../assets/Images/Tile 2.jpg';
+import Cover03 from '../../assets/Images/Tile 3.jpg';
+import Cover04 from '../../assets/Images/Tile 4.jpg';
+import Cover05 from '../../assets/Images/Tile 5.jpg';
+import Cover06 from '../../assets/Images/Tile 6.jpg';
+import UserHeader from './../../Components/Header/UserHeader';
+import AdminFooter from './../../Components/Footer/AdminFooter';
 
-    import { Link } from '@inertiajs/react';
-
-export default function CustomerDashboard() {
+const AdminDashboard = () => {
     return (
-        <div>
-            <h1>Admin Dashboard</h1>
-            <Link href="/logout" method="post" as="button">
-                Logout
-            </Link>
-        </div>
-    );
+        <>
+            <header>
+              <UserHeader />
+            </header>
+    
+           
+            <div className="px-3 pt-5 row gx-5">  
+              
+                  <div className="mb-4 col-md-4 position-relative"> 
+                    <div className="border-0 shadow cards">
+                      <div className="cards-img-top">
+                          <img src={Cover01} alt="Banner1" className="w-100"/>
+                          <Link href={route('admin.ongoingEvents')} className="image-overlay">Ongoing Events</Link>
+                      </div>
+                    </div>
+                  </div>
+    
+                  <div className="mb-4 col-md-4 position-relative">
+                    <div className="border-0 shadow cards">
+                      <div className="cards-img-top">
+                        <img src={Cover02} alt="Banner2" className="w-100"/>
+                        <Link to="/" className="image-overlay">Pending Requests</Link>
+                      </div>
+                  </div>
+                  </div>
+    
+                  <div className="mb-4 col-md-4 position-relative">
+                    <div className="border-0 shadow cards">
+                      <div className="cards-img-top">
+                          <img src={Cover03} alt="Banner3" className="w-100"/>
+                          <Link to="/" className="image-overlay">History</Link>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              
+                <div className="px-3 pt-5 row gx-5">  
+                    <div className="mb-4 col-md-4 position-relative"> 
+                        <div className="border-0 shadow cards">
+                            <div className="cards-img-top">
+                                <img src={Cover04} alt="Banner4" className="w-100"/>
+                                <Link href={route('admin.userDetails')} className="image-overlay">User Details</Link>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div className="mb-4 col-md-4 position-relative">
+                        <div className="border-0 shadow cards">
+                            <div className="cards-img-top">
+                            <img src={Cover05} alt="Banner5" className="w-100"/>
+                            <Link to="/" className="image-overlay">Reviews</Link>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div className="mb-4 col-md-4 position-relative">
+                        <div className="border-0 shadow cards">
+                            <div className="cards-img-top">
+                                <img src={Cover06} alt="Banner6" className="w-100"/>
+                                <Link to="/" className="image-overlay">Inquiries</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+    
+    
+            <footer>
+              <AdminFooter />
+              
+          </footer>
+          
+        </>
+    
+          
+      )
 }
+
+export default AdminDashboard
