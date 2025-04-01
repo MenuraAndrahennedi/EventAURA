a5c532df45eb70c54a1053a0c737e3ea4f40d084
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-04-01 10:15:24 +0530

    update programmerdashboard.jsx

--- resources/js/Pages/Programmer/ProgrammerDashboard.jsx
+++ resources/js/Pages/Programmer/ProgrammerDashboard.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-
-import UserHeader from '../../Components/Header/UserHeader';
-import AdminFooter from '../../Components/Footer/AdminFooter';
-
-import '../../../css/UserDashboard.scss';
+import "../../../css/UserDashboard.scss"
 import { Link } from '@inertiajs/react';
 
 import Cover01 from '../../assets/Images/Tile 1.jpg';
@@ -12,87 +8,89 @@ import Cover03 from '../../assets/Images/Tile 3.jpg';
 import Cover04 from '../../assets/Images/Tile 4.jpg';
 import Cover05 from '../../assets/Images/Tile 5.jpg';
 import Cover06 from '../../assets/Images/Tile 6.jpg';
+import UserHeader from './../../Components/Header/UserHeader';
+import AdminFooter from './../../Components/Footer/AdminFooter';
 
-
-
-const UserHome = () => {
+const ProgrammerDashboard = () => {
   return (
-    <>
-        <header>
-          <UserHeader />
-        </header>
-
-       
-        <div className="px-3 pt-5 row gx-5">  
-          
-              <div className="mb-4 col-md-4 position-relative"> 
-                <div className="border-0 shadow cards">
-                  <div className="cards-img-top">
-                      <img src={Cover01} alt="Banner1" className="w-100"/>
-                      <Link href={route('admin.ongoingEvents')} className="image-overlay">Ongoing Events</Link>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mb-4 col-md-4 position-relative">
-                <div className="border-0 shadow cards">
-                  <div className="cards-img-top">
-                    <img src={Cover02} alt="Banner2" className="w-100"/>
-                    <Link to="/" className="image-overlay">Pending Requests</Link>
-                  </div>
-              </div>
-              </div>
-
-              <div className="mb-4 col-md-4 position-relative">
-                <div className="border-0 shadow cards">
-                  <div className="cards-img-top">
-                      <img src={Cover03} alt="Banner3" className="w-100"/>
-                      <Link to="/" className="image-overlay">History</Link>
-                  </div>
-                </div>
-              </div>
-          </div>
-          
+    
+        <>
+            <header>
+              <UserHeader />
+            </header>
+    
+           
             <div className="px-3 pt-5 row gx-5">  
-                <div className="mb-4 col-md-4 position-relative"> 
+              
+                  <div className="mb-4 col-md-4 position-relative"> 
                     <div className="border-0 shadow cards">
-                        <div className="cards-img-top">
-                            <img src={Cover04} alt="Banner4" className="w-100"/>
-                            <Link href={route('admin.userDetails')} className="image-overlay">User Details</Link>
+                      <div className="cards-img-top">
+                          <img src={Cover01} alt="Banner1" className="w-100"/>
+                          <Link href={route('admin.ongoingEvents')} className="image-overlay">Ongoing Events</Link>
+                      </div>
+                    </div>
+                  </div>
+    
+                  <div className="mb-4 col-md-4 position-relative">
+                    <div className="border-0 shadow cards">
+                      <div className="cards-img-top">
+                        <img src={Cover02} alt="Banner2" className="w-100"/>
+                        <Link to="/" className="image-overlay">Pending Requests</Link>
+                      </div>
+                  </div>
+                  </div>
+    
+                  <div className="mb-4 col-md-4 position-relative">
+                    <div className="border-0 shadow cards">
+                      <div className="cards-img-top">
+                          <img src={Cover03} alt="Banner3" className="w-100"/>
+                          <Link to="/" className="image-overlay">History</Link>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              
+                <div className="px-3 pt-5 row gx-5">  
+                    <div className="mb-4 col-md-4 position-relative"> 
+                        <div className="border-0 shadow cards">
+                            <div className="cards-img-top">
+                                <img src={Cover04} alt="Banner4" className="w-100"/>
+                                <Link href={route('admin.userDetails')} className="image-overlay">User Details</Link>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div className="mb-4 col-md-4 position-relative">
+                        <div className="border-0 shadow cards">
+                            <div className="cards-img-top">
+                            <img src={Cover05} alt="Banner5" className="w-100"/>
+                            <Link to="/" className="image-overlay">Reviews</Link>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div className="mb-4 col-md-4 position-relative">
+                        <div className="border-0 shadow cards">
+                            <div className="cards-img-top">
+                                <img src={Cover06} alt="Banner6" className="w-100"/>
+                                <Link to="/" className="image-overlay">Inquiries</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div className="mb-4 col-md-4 position-relative">
-                    <div className="border-0 shadow cards">
-                        <div className="cards-img-top">
-                        <img src={Cover05} alt="Banner5" className="w-100"/>
-                        <Link to="/" className="image-overlay">Reviews</Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="mb-4 col-md-4 position-relative">
-                    <div className="border-0 shadow cards">
-                        <div className="cards-img-top">
-                            <img src={Cover06} alt="Banner6" className="w-100"/>
-                            <Link to="/" className="image-overlay">Inquiries</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-
-        <footer>
-          <AdminFooter />
-          <Link href={route('add.member')} >Add</Link>
-      </footer>
-      
-    </>
-
+    
+    
+    
+            <footer>
+              <AdminFooter />
+              
+          </footer>
+          
+        </>
+    
+          
       
   )
 }
 
-export default UserHome
+export default ProgrammerDashboard
