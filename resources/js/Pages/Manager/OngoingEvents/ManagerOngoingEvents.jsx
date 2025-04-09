bbd8b975f19c7de677e97b195fbb648d26457470
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-04-09 17:33:58 +0530

    updated ongoing events

--- resources/js/Pages/Manager/OngoingEvents/ManagerOngoingEvents.jsx
+++ resources/js/Pages/Manager/OngoingEvents/ManagerOngoingEvents.jsx
@@ -33,18 +33,18 @@ const ManagerOngoingEvents = () => {
          <SearchBar />
 
 
-            <section className = 'section-2 bg-light py-5'>
+            <section className = 'py-5 section-2 bg-light'>
             <div className = 'container'>
 
             {/*1st Row*/}
-            <div className = 'row pt-3'>
+            <div className = 'pt-3 row'>
              {events.map((event) => (   
-                <div className = 'col-md-4' key={event.id}>
-                <div className = 'card shadow border-0'>
+                <div className = 'col-md-3' key={event.id}>
+                <div className = 'border-0 shadow card'>
                     <div className = 'card-img-top'>
                         <img src = {event.image} alt={event.name} className = 'w-100'/>
                     </div>
-                    <div className = 'card-body p-4'>
+                    <div className = 'p-4 card-body'>
                     <div className='event-details'>
                         <p className='event-date-time' > 
                         {" "}
@@ -58,7 +58,7 @@ const ManagerOngoingEvents = () => {
                     <div className='event-title'>
                         <h2><b>{event.name}</b></h2>
                     </div>
-                    <div className = 'event-footer d-flex justify-content-between align-items-center mt-3'>
+                    <div className = 'mt-3 event-footer d-flex justify-content-between align-items-center'>
                         <p className='event-price'>
                         {event.bronze_ticket_price} 
                         LKR{" "}
@@ -74,11 +74,11 @@ const ManagerOngoingEvents = () => {
                 </div>
                 ))}
                 {/* <div className = 'col-md-4'>
-                <div className = 'card shadow border-0'>
+                <div className = 'border-0 shadow card'>
                     <div className = 'card-img-top'>
                         <img src = {Banner} alt="Banner1" className = 'w-100'/>
                     </div>
-                    <div className = 'card-body p-4'>
+                    <div className = 'p-4 card-body'>
                     <div className='event-details'>
                         <p className='event-date-time' >1st of April 2024 | 06.30 PM</p>
                         <p className='event-location'>National Youth Council</p>
@@ -86,7 +86,7 @@ const ManagerOngoingEvents = () => {
                     <div className='event-title'>
                         <h2><b>Sky Heroes</b></h2>
                     </div>
-                    <div className = 'event-footer d-flex justify-content-between align-items-center mt-3'>
+                    <div className = 'mt-3 event-footer d-flex justify-content-between align-items-center'>
                         <p className='event-price'>2,000 LKR <span className="price-subtext"><br/>upwards</span></p>
                         <Link to = '/ViewEvent' className='btn btn-primary'>View Event</Link>
                     </div>
@@ -94,11 +94,11 @@ const ManagerOngoingEvents = () => {
                 </div>
                 </div>
                 <div className = 'col-md-4'>
-                <div className = 'card shadow border-0'>
+                <div className = 'border-0 shadow card'>
                     <div className = 'card-img-top'>
                         <img src = {Banner} alt="Banner1" className = 'w-100'/>
                     </div>
-                    <div className = 'card-body p-4'>
+                    <div className = 'p-4 card-body'>
                         <div className='event-details'>
                         <p className='event-date-time' > 1st of April 2024 | 06.30 PM</p>
                         <p className='event-location'>National Youth Council</p>
@@ -106,7 +106,7 @@ const ManagerOngoingEvents = () => {
                         <div className='event-title'>
                         <h2><b>Sky Heroes</b></h2>
                         </div>
-                        <div className = 'event-footer d-flex justify-content-between align-items-center mt-3'>
+                        <div className = 'mt-3 event-footer d-flex justify-content-between align-items-center'>
                             <p className='event-price'>2,000 LKR <span className="price-subtext"><br/>upwards</span></p>
                             <Link to = '/ViewEvent' className='btn btn-primary'>View Event</Link>
                         </div>
@@ -116,13 +116,13 @@ const ManagerOngoingEvents = () => {
             </div>
 
             2nd Row
-            <div className = 'row pt-3'>
+            <div className = 'pt-3 row'>
                 <div className = 'col-md-4'>
-                <div className = 'card shadow border-0'>
+                <div className = 'border-0 shadow card'>
                     <div className = 'card-img-top'>
                         <img src = {Banner} alt="Banner1" className = 'w-100'/>
                     </div>
-                    <div className = 'card-body p-4'>
+                    <div className = 'p-4 card-body'>
                         <div className='event-details'>
                             <p className='event-date-time' >1st of April 2024 | 06.30 PM</p>
                             <p className='event-location'>National Youth Council</p>
@@ -130,7 +130,7 @@ const ManagerOngoingEvents = () => {
                         <div className='event-title'>
                             <h2><b>Sky Heroes</b></h2>
                         </div>
-                        <div className = 'event-footer d-flex justify-content-between align-items-center mt-3'>
+                        <div className = 'mt-3 event-footer d-flex justify-content-between align-items-center'>
                             <p className='event-price'>2,000 LKR <span className="price-subtext"><br/>upwards</span></p>
                             <Link to = '/ViewEvent' className='btn btn-primary'>View Event</Link>
                         </div>
@@ -138,11 +138,11 @@ const ManagerOngoingEvents = () => {
                 </div>
                 </div>
                 <div className = 'col-md-4'>
-                <div className = 'card shadow border-0'>
+                <div className = 'border-0 shadow card'>
                     <div className = 'card-img-top'>
                         <img src = {Banner} alt="Banner1" className = 'w-100'/>
                     </div>
-                    <div className = 'card-body p-4'>
+                    <div className = 'p-4 card-body'>
                     <div className='event-details'>
                         <p className='event-date-time' > 1st of April 2024 | 06.30 PM</p>
                         <p className='event-location'>National Youth Council</p>
@@ -150,7 +150,7 @@ const ManagerOngoingEvents = () => {
                         <div className='event-title'>
                         <h2><b>Sky Heroes</b></h2>
                         </div>
-                        <div className = 'event-footer d-flex justify-content-between align-items-center mt-3'>
+                        <div className = 'mt-3 event-footer d-flex justify-content-between align-items-center'>
                             <p className='event-price'>2,000 LKR <span className="price-subtext"><br/>upwards</span></p>
                             <Link to = '/ViewEvent' className='btn btn-primary'>View Event</Link>
                         </div>
@@ -158,11 +158,11 @@ const ManagerOngoingEvents = () => {
                 </div>
                 </div>
                 <div className = 'col-md-4'>
-                <div className = 'card shadow border-0'>
+                <div className = 'border-0 shadow card'>
                     <div className = 'card-img-top'>
                         <img src = {Banner} alt="Banner1" className = 'w-100'/>
                     </div>
-                    <div className = 'card-body p-4'>
+                    <div className = 'p-4 card-body'>
                     <div className='event-details'>
                         <p className='event-date-time' > 1st of April 2024 | 06.30 PM</p>
                         <p className='event-location'>National Youth Council</p>
@@ -170,7 +170,7 @@ const ManagerOngoingEvents = () => {
                         <div className='event-title'>
                         <h2><b>Sky Heroes</b></h2>
                         </div>
-                        <div className = 'event-footer d-flex justify-content-between align-items-center mt-3'>
+                        <div className = 'mt-3 event-footer d-flex justify-content-between align-items-center'>
                             <p className='event-price'>2,000 LKR <span className="price-subtext"><br/>upwards</span></p>
                             <Link to = '/ViewEvent' className='btn btn-primary'>View Event</Link>
                         </div>
