c787d7abd19802e4835e046a3fe99b892dfe26dd
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-04-17 15:15:40 +0530

    updated mainheader

--- resources/js/Components/Header/MainHeader.jsx
+++ resources/js/Components/Header/MainHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { Link } from "@inertiajs/react";
+import { Link,useState } from "@inertiajs/react";
 import Container from "react-bootstrap/Container";
 import "../../../css/style.scss";
 import Logo from "../../assets/Images/Logo.png";
@@ -55,3 +55,43 @@ const MainHeader = () => {
 };
 
 export default MainHeader;
+
+
+
+{/**
+    return (
+        <header>
+            <Navbar className="py-3 bg-dark navbar-dark" expand="lg">
+                <Container fluid>
+                    
+                    <Navbar.Brand as={Link} href={route('home')} className="logo">
+                        <img src={Logo} alt="Logo" className="logo-img" />
+                    </Navbar.Brand>
+
+                    
+                    <Nav className="flex-wrap ms-auto d-flex justify-content-center justify-content-lg-end">
+                        <Nav.Link as={Link} href={route('home')} className="px-3 text-white">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} href="/about" className="px-3 text-white">
+                            About US
+                        </Nav.Link>
+                        <Nav.Link as={Link} href={route('connect-with-us')} className="px-3 text-white">
+                            Contact US
+                        </Nav.Link>
+                        <Nav.Link as={Link} href={route('browse')} className="px-3 text-white">
+                            BROWSE EVENT
+                        </Nav.Link>
+                        <Nav.Link
+                            as={Link}
+                            href={route('event.create')}
+                            className="create-event-btn btn btn-info fw-bold ms-3"
+                            style={{ borderRadius: "20px", padding: "6px 16px" }}
+                        >
+                            CREATE EVENT
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </header>
+    ); */}
