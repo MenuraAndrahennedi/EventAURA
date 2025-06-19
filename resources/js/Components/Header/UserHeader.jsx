b78e71f71c4f3f18540b839990eb60e4ca6e6ef4
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-06-19 05:58:48 +0530

    updated userheader

--- resources/js/Components/Header/UserHeader.jsx
+++ resources/js/Components/Header/UserHeader.jsx
@@ -37,114 +37,68 @@ const UserHeader = () => {
        
             <Navbar expand="lg" className="bg-dark navbar-dark">
                  <Container fluid>
-                 <Navbar.Brand as={Link} href="/" className = 'logo'><img src={Logo} alt="Logo" className="logo-img" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <div id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link as={Link} href={dashboardRoute} className = 'nav-link'>Dashboard</Nav.Link>
-                    <Nav.Link as={Link} href="/ongoing" className = 'nav-link'>Ongoing events</Nav.Link>
-                    <Nav.Link as={Link} href="/event/create-requests" className = 'nav-link'>Pending requests</Nav.Link>
-                    <Nav.Link as={Link} href="/inquiries" className = 'nav-link'>Inquiries</Nav.Link>
-                    <Nav.Link as={Link} href="/other/profile-show" className = 'nav-link'>{userType}</Nav.Link>
+                  <Navbar.Brand as={Link} href="/" className = 'logo'><img src={Logo} alt="Logo" className="logo-img" /></Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <div id="basic-navbar-nav">
+                  <Nav className="ms-auto">
+                      <Nav.Link as={Link} href={dashboardRoute} className = 'nav-link'>Dashboard</Nav.Link>
+                      <Nav.Link as={Link} href="/ongoing" className = 'nav-link'>Ongoing events</Nav.Link>
+                      <Nav.Link as={Link} href="/event/create-requests" className = 'nav-link'>Pending requests</Nav.Link>
+                      <Nav.Link as={Link} href="/inquiries" className = 'nav-link'>Inquiries</Nav.Link>
+                      <Nav.Link as={Link} href="/other/profile-show" className = 'nav-link'>{userType}</Nav.Link>
                     
 
-                     {/* Profile Dropdown */}
-                     {/*<div style = {{ marginLeft:'500px' }}>
-            <Dropdown align="end" className="ms-3">
-              <Dropdown.Toggle
-                variant="secondary"
-                className="gap-2 text-white bg-transparent border-0 d-flex align-items-center"
-                id="dropdown-basic"
-              >
-                {user?.avatar ? (
-                  <Image
-                    src={`/storage/${user.avatar}`}
-                    roundedCircle
-                    width="30"
-                    height="30"
-                    alt="Profile"
-                  />
-                ) : (
-                  <div
-                    style={{
-                      width: '30px',
-                      height: '30px',
-                      borderRadius: '50%',
-                      backgroundColor: 'rgb(19, 185, 227)',
-                      color: 'white',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      fontWeight: 'bold',
-                    }}
-                  >
-                    {user?.name?.charAt(0).toUpperCase()}
+                    
+
+                    <div className="d-flex align-items-center ms-3">
+                          <Dropdown align="end">
+                            <Dropdown.Toggle
+                              variant="secondary"
+                              className="gap-2 text-white bg-transparent border-0 d-flex align-items-center"
+                              id="dropdown-basic"
+                            >
+                              {user?.avatar ? (
+                                <Image
+                                  src={`/storage/${user.avatar}`}
+                                  roundedCircle
+                                  width="30"
+                                  height="30"
+                                  alt="Profile"
+                                />
+                              ) : (
+                                <div
+                                  style={{
+                                    width: '30px',
+                                    height: '30px',
+                                    borderRadius: '50%',
+                                    backgroundColor: 'rgb(19, 185, 227)',
+                                    color: 'white',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    fontWeight: 'bold',
+                                  }}
+                                >
+                                  {user?.name?.charAt(0).toUpperCase()}
+                                </div>
+                              )}
+                              <span className="ms-2">{user?.name?.split(' ')[0]}</span>
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                              <Dropdown.Item as={Link} href="/other/profile-show">
+                                User Account
+                              </Dropdown.Item>
+                              <Dropdown.Divider />
+                              <Dropdown.Item as={Link} href={route('logout')} method="post">
+                                Logout
+                              </Dropdown.Item>
+                            </Dropdown.Menu>
+                          </Dropdown>
+                      </div>
+
+                    </Nav>
                   </div>
-                )}
-                <span>{user?.name?.split(' ')[0]}</span>
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} href="/other/profile-show">
-                  User Account
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item as={Link} href={route('logout')} method="post">
-                  Logout
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            </div>*/}
-
-            <div className="d-flex align-items-center ms-3">
-                  <Dropdown align="end">
-                    <Dropdown.Toggle
-                      variant="secondary"
-                      className="gap-2 text-white bg-transparent border-0 d-flex align-items-center"
-                      id="dropdown-basic"
-                    >
-                      {user?.avatar ? (
-                        <Image
-                          src={`/storage/${user.avatar}`}
-                          roundedCircle
-                          width="30"
-                          height="30"
-                          alt="Profile"
-                        />
-                      ) : (
-                        <div
-                          style={{
-                            width: '30px',
-                            height: '30px',
-                            borderRadius: '50%',
-                            backgroundColor: 'rgb(19, 185, 227)',
-                            color: 'white',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            fontWeight: 'bold',
-                          }}
-                        >
-                          {user?.name?.charAt(0).toUpperCase()}
-                        </div>
-                      )}
-                      <span className="ms-2">{user?.name?.split(' ')[0]}</span>
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                      <Dropdown.Item as={Link} href="/other/profile-show">
-                        User Account
-                      </Dropdown.Item>
-                      <Dropdown.Divider />
-                      <Dropdown.Item as={Link} href={route('logout')} method="post">
-                        Logout
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-               </div>
-
-                </Nav>
-                </div>
                  </Container>
             </Navbar>
         
