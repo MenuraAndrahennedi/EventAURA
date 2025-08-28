7ea230aa6d7eba98e764312c91f6fc5c1bda0cac
Imalsha Sathsarani <158012123+ImalshaSathsarani@users.noreply.github.com>
2025-08-28 16:15:11 +0530

    Solve the user profile issue

--- resources/js/Components/SideBar/TBSideBar.jsx
+++ resources/js/Components/SideBar/TBSideBar.jsx
@@ -1,23 +1,23 @@
-// import React from 'react'
-// import { Link } from "@inertiajs/react";
-// import '../../../css/profile.scss';
+import React from 'react'
+import { Link } from "@inertiajs/react";
+import '../../../css/profile.scss';
 
-// const TBSideBar = () => {
-//   return (
-//     <main>
-//     <div className="border-0 shadow card sidebar">
-//        <div className="p-4 card-body">
-//                <ul className="sidebar-menu">
-//                     <li><Link href={route('tb-profile')} className={route().current('tb-profile') ? 'active-link' : ''}>Profile Details</Link></li>
-//                     <li><Link href={route('TBPurchaseHistory')} className={route().current('TBPurchaseHistory') ? 'active-link' : ''}>Purchase History</Link></li>
-//                     <li><Link href={route('user.change-password')} className={route().current('user.change-password') ? 'active-link' : ''}>Change Password</Link></li>
-//                     <li><Link href={route('TBSignOut')} className={route().current('TBSignOut') ? 'active-link' : ''}>Sign Out</Link></li>
-//                </ul>
-//         </div>
-//      </div>
+const TBSideBar = () => {
+  return (
+    <main>
+    <div className="border-0 shadow card sidebar">
+       <div className="p-4 card-body">
+               <ul className="sidebar-menu">
+                    <li><Link href={route('tb-profile')} className={route().current('tb-profile') ? 'active-link' : ''}>Profile Details</Link></li>
+                    <li><Link href={route('TBPurchaseHistory')} className={route().current('TBPurchaseHistory') ? 'active-link' : ''}>Purchase History</Link></li>
+                    <li><Link href={route('user.change-password')} className={route().current('user.change-password') ? 'active-link' : ''}>Change Password</Link></li>
+                    <li><Link href={route('TBSignOut')} className={route().current('TBSignOut') ? 'active-link' : ''}>Sign Out</Link></li>
+               </ul>
+        </div>
+     </div>
    
-//  </main>
-//   )
-// }
+ </main>
+  )
+}
 
-// export default TBSideBar
+export default TBSideBar
